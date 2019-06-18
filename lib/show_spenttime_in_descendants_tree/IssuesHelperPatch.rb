ef923fae80c13e992b8bc234c60db90a5c3b4d29
Tarwirdur Turon <tarwirdur@ya.ru>
2019-06-18 14:12:50 +0300

    priority field added

--- lib/show_spenttime_in_descendants_tree/IssuesHelperPatch.rb
+++ lib/show_spenttime_in_descendants_tree/IssuesHelperPatch.rb
@@ -21,7 +21,8 @@ module IssuesHelperPatch
         row << content_tag('td', check_box_tag("ids[]", child.id, false, :id => nil), :class => 'checkbox') +
                  content_tag('td', link_to_issue(child, :project => (issue.project_id != child.project_id)), :class => 'subject', :style => 'width: 50%')
         row << content_tag('td', h(l_hours(child.total_spent_hours)) + "/" + h(child.estimated_hours)) unless issue.disabled_core_fields.include?('estimated_hours') || !User.current.allowed_to?(:view_time_entries, issue.project)
-        row << content_tag('td', h(child.status), :class => 'status') +
+        row << content_tag('td', h(child.priority), :class => 'priority') +
+                 content_tag('td', h(child.status), :class => 'status') +
                  content_tag('td', link_to_user(child.assigned_to), :class => 'assigned_to') +
                  content_tag('td', child.disabled_core_fields.include?('done_ratio') ? '' : progress_bar(child.done_ratio), :class=> 'done_ratio') +
                  content_tag('td', link_to_context_menu, :class => 'buttons')
